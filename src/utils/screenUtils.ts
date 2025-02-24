76ee482da8d447f372b3c5f33fe41ca753624163
SauceCode <LESLEY.EDINAM@GMAIL.COM>
2025-02-24 10:02:57 +0000

    Fix: Made window conditional

--- src/utils/screenUtils.ts
+++ src/utils/screenUtils.ts
@@ -1,4 +1,7 @@
 export const getCenterOfScreen = () => {
+  if (typeof window === 'undefined') {
+    return 0 // Return 0 or some fallback value during SSR (if you need it)
+  }
   const screenWidth = window.innerWidth
   return screenWidth / 2
 }
